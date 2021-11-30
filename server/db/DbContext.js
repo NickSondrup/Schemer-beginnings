c418c8d6d07dbcc67ff27de02e8817797a4ef39e
NickSondrup <nicholas.sondrup@gmail.com>
2021-11-29 23:24:21 -0700

    colors started

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 import { AccountSchema, ProfileSchema } from '../models/Account'
+import { ColorSchema } from '../models/Color.js'
 import { ProjectSchema } from '../models/Project.js'
 import { SectionSchema } from '../models/Section.js'
 import { ValueSchema } from '../models/Value'
@@ -10,6 +11,7 @@ class DbContext {
   Profiles = mongoose.model('Profile', ProfileSchema, 'accounts');
   Projects = mongoose.model('Project', ProjectSchema);
   Sections = mongoose.model('Section', SectionSchema);
+  Colors = mongoose.model('Color', ColorSchema);
 }
 
 export const dbContext = new DbContext()
---
+++ server/models/Color.js
@@ -0,0 +1,6 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+export const ColorSchema = new Schema({
+
+})
